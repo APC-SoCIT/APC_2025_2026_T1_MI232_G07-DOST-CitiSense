e15445359329dc89c05ea21f5f9141e4cf46ec22
lsbernal <lsbernal@student.apc.edu.ph>
2025-06-30 13:38:14 +0800

    Initial Commit

---
+++ mysite/drf/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Create your models here.
+class SentimentPost(models.Model):
+
+    GENDER_CHOICES = [
+        ("M","Male"),
+        ("F", "Female")
+    ]
+
+    SERVICE_CHOICES = [
+        ("Hybrid Seminar", "Hybrid Seminar"),
+        ("Material Requests", "Material Requests"),
+        ("Online Library", "Online Library"),
+        ("Library Tour","Library Tour")
+    ]
+    name = models.TextField(max_length=100)
+    service = models.CharField(max_length=100, choices=SERVICE_CHOICES)
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+    feedback = models.TextField(max_length=100)
+    sentiment = models.TextField(max_length=10)
+    
+    def __str__(self):
+        return self.name
