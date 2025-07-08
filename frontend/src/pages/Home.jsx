a4c208467083e0a6743740d23b70b90f1b6456f2
lsbernal <lsbernal@student.apc.edu.ph>
2025-07-08 22:57:03 +0800

    Added an api url for the backend, and added a test home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,24 @@
+import { useEffect, useState } from "react";
+import api from "../api";
+
 function Home() {
+    const [user, setUser] = useState(null)
+
+    useEffect(() => {
+        const getUser = async () => {
+        try{
+            const res = await api.get("/api/auth/user")
+            setUser(res.data.username)
+        }catch (error) {
+            alert(error)
+        }
+    }
+    getUser();
+}, []);
+
+
     return(
-        <div>Home</div>
+        <div>{user ?`Welcome ${user}` : "Loading..."}</div>
         
     ) 
 }
