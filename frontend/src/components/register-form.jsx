79b307ba57e62382f13f4729e8b9badd103652a3
lsbernal <lsbernal@student.apc.edu.ph>
2025-07-17 19:16:25 +0800

    Added a register form and logic for it

---
+++ frontend/src/components/register-form.jsx
@@ -0,0 +1,71 @@
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import api from "../api";
+
+export function RegisterForm({ className, route, ...props }) {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password1, setPassword1] = useState("");
+  const [password2, setPassword2] = useState("");
+  const [isLoading, setisLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setisLoading(true);
+
+    try {
+        const res = await api.post(route, {username, email, password1, password2})
+        navigate("/login")
+    } catch(error) {
+        alert(error)
+    } finally {
+        setisLoading(false)
+    }
+  };
+  return (
+    <form onSubmit={handleSubmit} className={cn("flex flex-col gap-6", className)} {...props}>
+      <div className="flex flex-col items-center gap-2 text-center">
+        <h1 className="text-2xl font-bold">Register an account</h1>
+        <p className="text-muted-foreground text-sm text-balance">
+          Enter your credentials to register an account
+        </p>
+      </div>
+      <div className="grid gap-6">
+        <div className="grid gap-3">
+          <Label htmlFor="username">Username</Label>
+          <Input id="username" value={username} type="text" onChange={(e) => setUsername(e.target.value)}required />
+        </div>
+        <div className="grid gap-3">
+          <Label htmlFor="email">Email</Label>
+          <Input id="email" value={email} type="email" placeholder="m@example.com" onChange={(e) => setEmail(e.target.value)}required />
+        </div>
+        <div className="grid gap-3">
+          <div className="flex items-center">
+            <Label htmlFor="password">Password</Label>
+          </div>
+          <Input id="password1" value={password1} type="password" onChange={(e) => setPassword1(e.target.value)} required />
+        </div>
+        <div className="grid gap-3">
+          <div className="flex items-center">
+            <Label htmlFor="password">Confirm Passsword</Label>
+          </div>
+          <Input id="password2" value={password2} type="password" onChange={(e) => setPassword2(e.target.value)}required />
+        </div>
+        <Button type="submit" className="w-full" disabled={isLoading}>
+          Register
+        </Button>
+      </div>
+      <div className="text-center text-sm">
+        Already have an account?{" "}
+        <Link to="/login" className="underline underline-offset-4">
+          Sign in
+        </Link>
+      </div>
+    </form>
+  );
+}
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,15 +1,48 @@
-import RegisterForm from "../components/RegisterForm";
+import { GalleryVerticalEnd } from "lucide-react"
+import { RegisterForm } from "../components/register-form"
+import { Link } from "react-router-dom"
 
-
-function Register() {
-    return(
-        <main className="flex flex-col items-center">
-            <div className="mt-5 justify-center">
-                <RegisterForm route="/api/auth/register/"/>
+export default function RegisterPage() {
+  return (
+    <div className="grid min-h-svh lg:grid-cols-[1fr_5fr]">
+      <div className="bg-muted relative hidden lg:block">
+        <img
+          src="/blue1.png"
+          alt="Image"
+          className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+        />
+      </div>
+      <div className="flex flex-col gap-4 p-6 md:p-10">
+        <div className="flex justify-center gap-2 md:justify-start">
+          <Link to="/login" className="flex items-center gap-2 font-medium">
+            <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
+              <GalleryVerticalEnd className="size-4" />
             </div>
-        </main>
-    ) 
-    
+            QuadThink
+          </Link>
+        </div>
+        <div className="flex flex-1 items-center justify-center">
+          <div className="w-full max-w-xs">
+            <RegisterForm route="/api/auth/register/" />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
-export default Register;
+// import RegisterForm from "../components/RegisterForm";
+
+
+// function Register() {
+//     return(
+//         <main className="flex flex-col items-center">
+//             <div className="mt-5 justify-center">
+//                 <RegisterForm route="/api/auth/register/"/>
+//             </div>
+//         </main>
+//     ) 
+    
+// }
+
+// export default Register;
