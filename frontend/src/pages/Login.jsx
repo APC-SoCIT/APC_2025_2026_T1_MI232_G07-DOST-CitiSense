ac7a751f1b964ab7c4665c885e649fa01a047067
lsbernal <lsbernal@student.apc.edu.ph>
2025-07-17 18:18:50 +0800

    added a login form ui (2nd/finalized version)

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,14 +1,34 @@
-import LoginForm from "../components/LoginForm";
+import { GalleryVerticalEnd } from "lucide-react"
+import { Link } from "react-router-dom"
+import { LoginForm1 } from "@/components/login-form"
 
-function Login() {
-    return(
-        <main className="flex flex-col items-center">
-            <div className="mt-5 justify-center">
-                <LoginForm route="/api/auth/token/"/>
-            </div>
-        </main>
-    ) 
-    
+export default function LoginPage() {
+  return (
+    <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <Link to="/" className="flex items-center gap-2 self-center font-medium">
+          <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
+            <GalleryVerticalEnd className="size-4" />
+          </div>
+          QuadThink
+        </Link>
+        <LoginForm1 />
+      </div>
+    </div>
+  )
 }
 
-export default Login;
+// import LoginForm from "../components/LoginForm";
+
+// function Login() {
+//     return(
+//         <main className="flex flex-col items-center">
+//             <div className="mt-5 justify-center">
+//                 <LoginForm route="/api/auth/token/"/>
+//             </div>
+//         </main>
+//     ) 
+    
+// }
+
+// export default Login;
