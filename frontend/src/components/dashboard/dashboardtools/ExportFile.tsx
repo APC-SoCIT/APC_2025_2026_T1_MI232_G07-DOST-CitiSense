06862cb528328edf0251c109a7b2eb8a1da9f257
markmarabi <mcmarabi@student.apc.edu.ph>
2026-02-15 23:00:11 +0800

    add export file component

--- frontend/src/components/dashboard/dashboardtools/ExportFile.tsx
+++ frontend/src/components/dashboard/dashboardtools/ExportFile.tsx
@@ -1,7 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
+import PizZip from "pizzip";
+import Docxtemplater from "docxtemplater";
+import PizZipUtils from "pizzip/utils/index.js";
+import { saveAs } from "file-saver";
+import { Button } from "../../ui/button";
+import { Download } from "lucide-react";
 
-const ExportFile = () => {
-  return <div>ExportFile</div>;
+function loadFile(url, callback) {
+  PizZipUtils.getBinaryContent(url, callback);
+}
+
+export const ExportFile = () => {
+  const [isExporting, setIsExporting] = useState(false);
+  const downloadTemplate = async () => {
+    loadFile(
+      "/templates/ReportTemplate.docx",
+      function (error: string, content) {
+        if (error) {
+          throw error;
+        }
+        const zip = new PizZip(content);
+        const doc = new Docxtemplater(zip, {
+          paragraphLoop: true,
+          linebreaks: true,
+        });
+        doc.render({
+          date_now: `${new Date().toLocaleDateString()}, ${new Date().toLocaleTimeString()}`,
+        });
+        const out = doc.getZip().generate({
+          type: "blob",
+          mimeType:
+            "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+        }); //Output the document using Data-URI
+        saveAs(out, "output.docx");
+      },
+    );
+  };
+
+  return (
+    <Button
+      variant="outline"
+      className="w-full sm:w-auto bg-green-600 hover:bg-green-700 hover:text-white text-white"
+      onClick={downloadTemplate}
+    >
+      <Download className="mr-2 h-4 w-4" />
+      Export
+    </Button>
+  );
 };
-
-export default ExportFile;
