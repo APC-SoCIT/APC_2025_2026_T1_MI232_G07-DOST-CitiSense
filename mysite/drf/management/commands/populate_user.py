b0b283c9090ea717768acc3afbf6290de1c139c1
lsbernal <lsbernal@student.apc.edu.ph>
2026-09-07 09:48:05 +0800

    chore: add verified email to superuser1 and add user2 to populate_user command

--- mysite/drf/management/commands/populate_user.py
+++ mysite/drf/management/commands/populate_user.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.core.management.base import BaseCommand
+from allauth.account.models import EmailAddress
 
 User = get_user_model()
 
@@ -11,10 +12,15 @@ class Command(BaseCommand):
         try:     
             # Check if the superuser already exists:
             if not User.objects.filter(username="superuser1").exists():
-                superuser = User.objects.create_user("superuser1", password="superuser1")
+                superuser = User.objects.create_user("superuser1", email= "superuser1@stii.dost.gov.ph", password="superuser1")
                 superuser.is_superuser = True
                 superuser.is_staff = True
+                superuser.is_registered=True
                 superuser.save()
+
+                # For successfully verifying the email of the user to login to CitiSense
+                EmailAddress.objects.create(user=superuser, email=superuser.email, verified=True, primary=True)
+                
                 self.stdout.write(self.style.SUCCESS("Created superuser!"))
             else:
                 self.stderr.write(self.style.ERROR(f'Superuser already exists!'))
@@ -27,6 +33,22 @@ class Command(BaseCommand):
 
             else:
                 self.stderr.write(self.style.ERROR("Normal user already exists!"))
+
+            if not User.objects.filter(username="user2").exists():
+                user2 = User.objects.create_user(
+                    "user2",
+                    email="user2@stii.dost.gov.ph",
+                    password="Password123!",
+                )
+                user2.is_registered = True
+                user2.save()
+
+                # For successfully verifying the email of the user to login to CitiSense
+                EmailAddress.objects.create(user=user2, email=user2.email, verified=True, primary=True)
+
+                self.stdout.write(self.style.SUCCESS("Created user2!"))
+            else:
+                self.stderr.write(self.style.ERROR("User 2 already exists!"))
 
         except Exception as e:
             self.stderr.write(self.style.ERROR(f'Error populating users {e}'))
