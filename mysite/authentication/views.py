26d24c3cf49b31e4f1c093e436c017077da417dc
lsbernal <lsbernal@student.apc.edu.ph>
2026-09-19 18:37:11 +0800

    chore: upgrade python deps, fix google oauth client, set timezone to manila

--- mysite/authentication/views.py
+++ mysite/authentication/views.py
@@ -12,8 +12,6 @@ from .throttles import AuthThrottle, PasswordResetThrottle, EmailVerificationThr
 
 User = get_user_model()
 
-#fix for "OAuth2Client.__init__() got multiple values for argument 'scope_delimiter'" error
-#link to solution: https://github.com/iMerica/dj-rest-auth/issues/673#issuecomment-2726403865
 class CustomGoogleOAuth2Client(OAuth2Client):
     def __init__(
         self,
@@ -23,7 +21,7 @@ class CustomGoogleOAuth2Client(OAuth2Client):
         access_token_method,
         access_token_url,
         callback_url,
-        _scope,  # This is fix for incompatibility between django-allauth==65.3.1 and dj-rest-auth==7.0.1
+        _scope=None,
         scope_delimiter=" ",
         headers=None,
         basic_auth=False,
